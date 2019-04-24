e3ca03b680c7e27e395a530578b878ed158dc433
dbusteed <busteed.davis@gmail.com>
2019-04-24 00:03:35 -0600

    stuff

--- kickstarter_project/clean_kickstarter.py
+++ kickstarter_project/clean_kickstarter.py
@@ -42,18 +42,12 @@ for counter,csv in enumerate(files):
           
         cat = json.loads(r['category'])['slug'].split('/')
         category.append(cat[0])
-        # try:
-            # subcategory.append(cat[1])
-        # except:
-              # subcategory.append(' ')
-              # rem.append(i)
       
     df['month'] = [datetime.fromtimestamp(ts).strftime('%b') for ts in df['launched_at']]
-    df = df[['pledged', 'backers_count', 'goal', 'month', 'state']]
+    df = df[['pledged', 'backers_count', 'goal', 'month']]
       
     df['international'] = inte
     df['category'] = category
-    # df['subcategory'] = subcategory # drop, as well as deadline, launched_at, state, and maybe add sent
     df['days_open'] = days_open
     df['title_len'] = title_len
     df['blurb_len'] = blurb_len
@@ -80,38 +74,3 @@ for counter,csv in enumerate(files):
         header = False
         
     print(f'processed {counter+1}/{len(files)}')
-
-    # array(['film & video', 'music', 'games', 'crafts', 'art', 'publishing',
-       # 'food', 'technology', 'fashion', 'comics', 'design', 'theater',
-       # 'photography', 'journalism', 'dance'], dtype=object)  15
-    
-
-    
-# cat_comics .0423998 ###
-# cat_crafts .0287635 ###
-# cat_dance .0132242 ###
-# cat_design  .040822  # + crafts
-# cat_fashion .0576517 
-# cat_film__~o .1378101 + photography
-# cat_food .0782552 
-# cat_games  .0678505  
-# cat_journa~m  .0249356 ###
-# cat_music  .1366281 
-# cat_photog~y  .0363814  ###
-# cat_publis~g   .0997262  + journalism + comics
-# cat_techno~y   .104427 
-# cat_theater .0319571 # + dance
-
-# cat_art 0.0991676
-
-
-# cat_fashion .0576517 
-# cat_food .0782552 
-# cat_games  .0678505  
-# cat_music 
-# cat_techno~y   
-# cat_design_crafts
-# cat_publis~g_journal_comics
-# cat_film__photography
-# cat_theater_dance
-# cat_art 0.0991676 (EXCLUDED)
